8e8a8873438a9a563dbc0c7e45aaa2123f154a76
Emil <emilskovbo18@gmail.com>
2020-05-05 13:35:06 +0200

    ipython

--- HentData.py
+++ HentData.py
@@ -1,16 +1,15 @@
-import numpy as np 
 import pandas as pd 
 import requests
-r = requests.get("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", allow_redirects=True)
+r = requests.get("https://covid.ourworldindata.org/data/owid-covid-data.csv", allow_redirects=True)
 open("corona_data.csv", 'wb').write(r.content)
-
+#"""https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"""
 corona_data = pd.read_csv("corona_data.csv",sep= ',')
 
 print(corona_data)
-data = corona_data[corona_data["countriesAndTerritories"]=="Afghanistan"]
+#data = corona_data[corona_data["countriesAndTerritories"]=="Afghanistan"]
 
-print(data)
-death = 0
-for counter in range(len(data)):
-    death += data["deaths"][counter]
-print(death)
+#print(data)
+#death = 0
+#for counter in range(len(data)):
+#    death += data["deaths"][counter]
+#print(death)
