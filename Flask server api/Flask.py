79b8734207cf614aaeb90661d0f76c1659ebd5c6
Emil <emilskovbo18@gmail.com>
2020-05-20 11:03:13 +0200

    flask server up and running

---
+++ Flask server api/Flask.py
@@ -0,0 +1,67 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request
+#import pymysql
+import mysql.connector
+from mysql.connector import Error
+
+
+app = Flask(__name__)
+#http://127.0.0.1:5000/corona/api/all
+@app.route('/corona/api/all', methods=['GET'])
+def get_all():
+    connection = mysql.connector.connect(host='localhost', port=3306, database='pythonexam', user='dev',password="ax2")
+    cursor = connection.cursor()
+    query = ("SELECT country, total_cases, New_cases, Total_deaths, New_deaths, Total_recov from corona20200517")
+    cursor.execute(query)
+    tasks = []
+    for country,total_cases,New_cases,Total_deaths,New_deaths,Total_recov in cursor:
+        tasks.append(
+        {
+        'Country': country,
+        'Total_cases': total_cases,
+        'New_cases': New_cases,
+        'Total_deaths': Total_deaths,
+        'New_deaths': New_deaths,
+        'Total_recov': Total_recov
+        },
+        )
+    #print(tasks)
+    cursor.close()
+    connection.close()
+    return jsonify({'tasks': tasks})
+#http://127.0.0.1:5000/corona/api/country/USA
+@app.route('/corona/api/country/<string:name>', methods=['GET'])
+def get_countryByName(name):
+    connection = mysql.connector.connect(host='localhost', port=3306, database='pythonexam', user='dev',password="ax2")
+    cursor = connection.cursor()
+    query = ("SELECT country, total_cases, New_cases, Total_deaths, New_deaths, Total_recov from corona20200517 WHERE country = %s")
+    where = name, 
+    cursor.execute(query, where)
+    tasks = []
+    for country,total_cases,New_cases,Total_deaths,New_deaths,Total_recov in cursor:
+        tasks.append(
+        {
+        'Country': country,
+        'Total_cases': total_cases,
+        'New_cases': New_cases,
+        'Total_deaths': Total_deaths,
+        'New_deaths': New_deaths,
+        'Total_recov': Total_recov
+        },
+        )
+    #print(tasks)
+    cursor.close()
+    connection.close()
+    return jsonify({'tasks': tasks})
+
+
+a = """@app.route('/corona/api/data/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]}) """
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
