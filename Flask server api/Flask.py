46755d1efcc1e1a7830a7e24e980486295a1ec53
fred8728 <fskn@hotmail.com>
2020-05-22 12:44:48 +0200

    cleaning for exam

--- Flask server api/Flask.py
+++ Flask server api/Flask.py
@@ -1,17 +1,15 @@
-## remove new cases 
+## NOTE: Ændrer table så den tager den seneste dato 
 
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request
-#import pymysql
-import mysql.connector
-from mysql.connector import Error
+import mysql.connector as connector
 
 
 app = Flask(__name__)
 #http://127.0.0.1:5000/corona/api/all
 @app.route('/corona/api/all', methods=['GET'])
 def get_all():
-    connection = mysql.connector.connect(host='localhost', port=3307, database='pythonexam', user='dev',password="ax2")
+    connection = connector.connect(host='localhost', port=3307, database='pythonexam', user='dev',password="ax2")
     cursor = connection.cursor()
     query = ("SELECT country, total_cases, New_cases, Total_deaths, New_deaths, Total_recov from corona20200517")
     cursor.execute(query)
@@ -35,7 +33,7 @@ def get_all():
 #http://127.0.0.1:5000/corona/api/country/USA
 @app.route('/corona/api/country/<string:name>', methods=['GET'])
 def get_countryByName(name):
-    connection = mysql.connector.connect(host='localhost', port=3307, database='pythonexam', user='dev',password="ax2")
+    connection = connector.connect(host='localhost', port=3307, database='pythonexam', user='dev',password="ax2")
     cursor = connection.cursor()
     query = ("SELECT country, total_cases, New_cases, Total_deaths, New_deaths, Total_recov from corona20200517 WHERE country = %s")
     where = name, 
@@ -57,13 +55,6 @@ def get_countryByName(name):
     connection.close()
     return jsonify({'tasks': tasks})
 
-
-a = """@app.route('/corona/api/data/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]}) """
 if __name__ == '__main__':
     app.run(debug=True)
 
